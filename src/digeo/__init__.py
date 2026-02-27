cbb1165a46abceb87a870d40954280f2448e3ed5
Hippolyte Verninas <55251414+Etyl@users.noreply.github.com>
2026-02-27 17:35:30 +0100

    DOC readthedocs integration (#3)

--- src/digeo/__init__.py
+++ src/digeo/__init__.py
@@ -1,7 +1,7 @@
 from digeo.mesh import Mesh, MeshBatch, MeshPoint, MeshPointBatch
 from digeo.mesh_loader import load_mesh_from_file, load_mesh_from_trimesh
 
-__version__ = "0.0.4"
+__version__ = "0.0.5dev"
 __all__ = [
     "Mesh",
     "MeshBatch",
