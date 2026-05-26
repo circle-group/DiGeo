d2d3c3b18edf36ea322bdf4a8ff5d03874219371
Hippolyte Verninas <etyl.sani@gmail.com>
2026-05-26 11:04:47 +0200

    RELEASE 1.0.1

--- src/digeo/__init__.py
+++ src/digeo/__init__.py
@@ -1,7 +1,7 @@
 from digeo.mesh import Mesh, MeshBatch, MeshPoint, MeshPointBatch
 from digeo.mesh_loader import load_mesh_from_file, load_mesh_from_trimesh
 
-__version__ = "1.0.1dev"
+__version__ = "1.0.1"
 __all__ = [
     "Mesh",
     "MeshBatch",
